836f64057f14d6064c0ffa62c658583987c2a0f7
Patrycja Petryk <patrycja.petryk@gmail.com>
2022-03-21 08:42:32 +0100

    Add slider on mobile

--- src/js/slider.js
+++ src/js/slider.js
@@ -5,6 +5,7 @@ const galleryDescriptions = document.querySelectorAll(
   '.home-page__description--js',
 );
 const queryWidth = window.matchMedia('(min-width: 1024px)');
+let changeImagesOnTimeoutTimer;
 let countImages = 0;
 
 const changeImagesOnMousemove = (event) => {
@@ -26,11 +27,30 @@ const changeImagesOnMousemove = (event) => {
   }
 };
 
+const changeImagesOnTimeout = () => {
+  for (let i = 0; i < imagesLength; i++) {
+    if (i == countImages) {
+      images[i].style.opacity = 1;
+    } else {
+      images[i].style.opacity = 0;
+    }
+  }
+  if (countImages < imagesLength - 1) {
+    countImages++;
+  } else {
+    countImages = 0;
+  }
+
+  changeImagesOnTimeoutTimer = setTimeout(changeImagesOnTimeout, 1000);
+};
+
 const mediaQuery = () => {
+  clearTimeout(changeImagesOnTimeoutTimer);
   if (queryWidth.matches) {
     slider.addEventListener('mousemove', changeImagesOnMousemove);
   } else {
     slider.removeEventListener('mousemove', changeImagesOnMousemove);
+    changeImagesOnTimeout();
   }
 };
 window.addEventListener('load', mediaQuery);
